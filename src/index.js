c816fa25a1453305d25f63b54c2da4dba3f9f364
Yuri0494 <76704347+Yuri0494@users.noreply.github.com>
2021-08-15 17:04:15 +0300

    fix: fixed all functions for comlpete kata

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 
 exports.min = function min (array) {
-
-  return array.sort((a, b) => a-b);
+  if(typeof(array) !== "object") {
+    return 0
+  }
+  if(array.length <= 1) {
+    return 0
+  }
+  return array.sort((a, b) => a-b)[0];
 }
 
 exports.max = function max (array) {
-  return array.sort((a, b) => b-a);
+  if(typeof(array) !== "object") {
+    return 0
+  }
+  if(array.length <= 1) {
+    return 0
+  }
+  return array.sort((a, b) => b-a)[0];
 }
 
 exports.avg = function avg (array) {
   let res = 0;
+  if(typeof(array) !== "object") {
+    return 0
+  }
+  if(array.length <= 1) {
+    return 0
+  }
   array.forEach(i => res+=i)
   return res / array.length;
 }
